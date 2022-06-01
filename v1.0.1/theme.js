2814262c9bd575117741b4bdb5d9d2cce2f5e903
Benjamin Kim <benjaminkim.email@gmail.com>
2022-05-31 22:09:17 -0400

    CSS Finished, Working on Settings

--- v1.0.1/theme.js
+++ v1.0.1/theme.js
@@ -1,8 +1,61 @@
-function LightenDarkenColor(col, amt) {
-    var num = parseInt(col, 16);
-    var r = (num >> 16) + amt;
-    var b = ((num >> 8) & 0x00FF) + amt;
-    var g = (num & 0x0000FF) + amt;
-    var newColor = g | (b << 8) | (r << 16);
-    return newColor.toString(16);
-  }
+const invertHex = hex => {
+	hex=parseInt(Number(hex.replace('#', '0x')), 10)
+  hex=~hex
+  hex=hex>>>0
+  hex=hex&0x00ffffff
+  hex='#' + hex.toString(16).padStart(6, "0")
+  
+  return hex
+}
+
+const adjust = (hex, amount) => {
+  return '#' + hex.replace(/^#/, '').replace(/../g, hex => ('0'+Math.min(255, Math.max(0, parseInt(hex, 16) + amount)).toString(16)).substr(-2));
+}
+
+localStorage.color_primary = localStorage.color_primary || "#3F88C5";
+localStorage.color_primary_light = localStorage.color_primary_light || (localStorage.color_primary ? adjust(localStorage.color_primary, 100) : "#a3ecff");
+localStorage.color_primary_hover = localStorage.color_primary_hover || (localStorage.color_primary ? adjust(localStorage.color_primary, -30) : "#216aa7");
+localStorage.color_secondary = localStorage.color_secondary || "#032B43";
+localStorage.color_accent = localStorage.color_accent || "#ffffff";
+localStorage.color_accent_reversed = localStorage.color_accent_reversed || (localStorage.color_accent ? invertHex(localStorage.color_accent) : "#000000");
+
+document.body.style = `
+  --primary-color: ${localStorage.color_primary};
+  --primary-light-color: ${localStorage.color_primary_light};
+  --primary-hover-color: ${localStorage.color_primary_hover};
+  --secondary-color: ${localStorage.color_secondary};
+  --accent-color: ${localStorage.color_accent};
+  --accent-reversed-color: ${localStorage.color_accent_reversed};
+`;
+
+const loop = () => {
+  document.querySelectorAll(".LeftSidePanel_logo__3qdoA svg g").forEach(element => {
+    element.innerHTML = `<use xlink:href="#logo-hapara-small-black"></use>`;
+  });
+
+  document.querySelectorAll(".ToolbarPersonaSettingsThemes_root__3w1cV").forEach(element => {
+    const settings = `
+    <div class="settings">
+      <h2>Customize</h2>
+      <label> Primary Color <input type="color" value="${localStorage.color_primary}"></label>
+      <br>
+      <label> Secondary Color <input type="color" value="${localStorage.color_secondary}"></label>
+      <br>
+      <label> Accent Color <input type="color" value="${localStorage.color_accent}"></label>
+      <br>
+      <button class="btn"> Confirm </button>
+      <br>
+      <h3>Advanced Setting ⤸</h3>
+      <div class="settings__advanced">
+      
+      
+      </div>
+    </div>
+    `
+    if (element.innerHTML === settings) return;
+    element.innerHTML = settings;
+  });
+
+  requestAnimationFrame(loop);
+}
+requestAnimationFrame(loop);
